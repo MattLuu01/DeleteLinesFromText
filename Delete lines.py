e1821e638fe7f49711413ca74671929fd2edd7ff
MattLuu01 <matthew_luu@yahoo.com>
2020-12-12 14:07:00 -0500

    Add files via upload

---
+++ Delete lines.py
@@ -0,0 +1,12 @@
+import glob   
+path ='/Users/Matthew/Desktop/College Work/Text/*.txt'   
+files=glob.glob(path)
+min = int(input("1st line to stop deleting: "))
+max = int(input("2nd line to start deleting after: "))
+for file in files: 
+    print(file)
+    infile = open(file,'r').readlines()
+    with open(file,'w') as outfile:
+        for index,line in enumerate(infile):
+            if index >= min and index <max:
+                outfile.write(line)
