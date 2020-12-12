41fadd7c2c741763759504856f4cf19a6a0bb82e
MattLuu01 <matthew_luu@yahoo.com>
2020-12-12 14:07:45 -0500

    Update Delete lines.py

--- Delete lines.py
+++ Delete lines.py
@@ -1,5 +1,5 @@
 import glob   
-path ='/Users/Matthew/Desktop/College Work/Text/*.txt'   
+path ='/Users/Matthew/Desktop/College Work/Text/*.txt'   #change this code to match location of path
 files=glob.glob(path)
 min = int(input("1st line to stop deleting: "))
 max = int(input("2nd line to start deleting after: "))
